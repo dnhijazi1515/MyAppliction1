c03589d7434a88fd09b25b68ded2c5f565233220
97252 <dnhijazi15108@gmail.com>
2024-10-05 22:21:50 +0300

    adanhijazi

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.myapplication1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tvWelcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvWelcome=findViewById(R.id.textView);
     }
+    public void change_text(View view) {
+        tvWelcome.setText("This is my first app");
+    }
+
+    public void finishApp(View view) {
+        finish();
+        System.exit(0);
+    }
+
+
+
 }
